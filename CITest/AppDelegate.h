a4a3c8cfd04a4c47411b3bff48310e93ae27f9e3
feng.lee <feng.lee@shendun.info>
2017-07-20 09:51:47 +0800

    dev

---
+++ CITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CITest
+//
+//  Created by yeye on 20/07/2017.
+//  Copyright © 2017 yeye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CITest
+//
+//  Created by yeye on 20/07/2017.
+//  Copyright © 2017 yeye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
